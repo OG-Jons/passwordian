347c7cdef062a3226b91560f021c41b71e0bd319
OG-Jons <jonas@marschall.pro>
2022-06-17 08:42:23 +0200

    added passwords type

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
+import { PasswordsModule } from './passwords/passwords.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig)],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), PasswordsModule],
   controllers: [AppController],
   providers: [AppService],
 })
