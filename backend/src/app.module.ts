9ee4ab7ebed2b792b5f96a37d550b297dc7c50c9
OG-Jons <jonas@marschall.pro>
2022-06-24 10:47:26 +0200

    formatting

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,14 +3,16 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
 import { PasswordsModule } from './passwords/passwords.module';
 import { AuthModule } from './auth/auth.module';
-import { DataSource } from 'typeorm';
 import { CategoriesModule } from './categories/categories.module';
 import { EncryptionService } from './encryption/encryption.service';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), PasswordsModule, AuthModule, CategoriesModule],
+  imports: [
+    TypeOrmModule.forRoot(typeOrmConfig),
+    PasswordsModule,
+    AuthModule,
+    CategoriesModule,
+  ],
   providers: [EncryptionService],
 })
-export class AppModule {
-  constructor(private dataSource: DataSource) {}
-}
+export class AppModule {}
