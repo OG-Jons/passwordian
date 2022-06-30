33456d1fccb9fbd82471bec00a349a7b42311fbf
OG-Jons <jonas@marschall.pro>
2022-06-30 21:11:45 +0200

    created update-master-password endpoint

---
+++ backend/src/auth/dto/update-password.dto.ts
@@ -0,0 +1,22 @@
+import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
+
+export class UpdatePasswordDto {
+  @IsString()
+  @MinLength(3)
+  @MaxLength(20)
+  username: string;
+
+  @IsString()
+  @MinLength(8)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'password too weak',
+  })
+  password: string;
+
+  @IsString()
+  @MinLength(8)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'password too weak',
+  })
+  newPassword: string;
+}
