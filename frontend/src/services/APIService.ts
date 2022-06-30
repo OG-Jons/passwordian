24f51d8174002938b2d042ea8501854d14efe239
OG-Jons <jonas@marschall.pro>
2022-06-30 20:55:27 +0200

    working api-service

--- frontend/src/services/APIService.ts
+++ frontend/src/services/APIService.ts
@@ -7,54 +7,75 @@ const extract = (data: any) => {
 
 /* Password section */
 
-export const getUserPasswords = async () => {
+export const getUserPasswords = async (): Promise<Password[]> => {
   return http.get("/passwords").then(extract);
 };
 
-export const getPassword = async (id: number) => {
+export const getPassword = async (id: number): Promise<Password> => {
   return http.get(`/passwords/${id}`).then(extract);
 };
 
-export const createUserPassword = async (password: Password) => {
+export const createUserPassword = async (
+  password: Password
+): Promise<Password> => {
   return http.post("/passwords", { ...password }).then(extract);
 };
 
-export const deleteUserPassword = async (id: number) => {
+export const deleteUserPassword = async (id: number): Promise<null> => {
   return http.delete(`/passwords/${id}`).then(extract);
 };
 
-export const updateUserPassword = async (id: number, password: Password) => {
+export const updateUserPassword = async (
+  id: number,
+  password: Password
+): Promise<Password> => {
   return http.put(`/passwords/${id}`, { ...password }).then(extract);
 };
 
 /* Auth section */
 
-export const login = async (username: string, password: string) => {
+export const login = async (
+  username: string,
+  password: string
+): Promise<{ accessToken: string }> => {
   return http.post("/auth/signin", { username, password }).then(extract);
 };
 
-export const register = async (username: string, password: string) => {
+export const register = async (
+  username: string,
+  password: string
+): Promise<null> => {
   return http.post("/auth/signup", { username, password }).then(extract);
+};
+
+export const updateMasterPassword = async (password: string): Promise<null> => {
+  throw new Error("Method not implemented.");
+  //return http.put("/auth/master", { password }).then(extract);
 };
 
 /* Category section */
 
-export const getUserCategories = async () => {
+export const getUserCategories = async (): Promise<Category[]> => {
   return http.get("/categories").then(extract);
 };
 
-export const getCategory = async (id: number) => {
+export const getCategory = async (id: number): Promise<Category> => {
   return http.get(`/categories/${id}`).then(extract);
 };
 
-export const createUserCategory = async (category: Category) => {
+export const createUserCategory = async (
+  category: Category
+): Promise<Category> => {
   return http.post("/categories", { ...category }).then(extract);
 };
 
-export const deleteUserCategory = async (id: number) => {
+export const deleteUserCategory = async (id: number): Promise<null> => {
   return http.delete(`/categories/${id}`).then(extract);
 };
 
-export const updateUserCategory = async (id: number, category: Category) => {
+export const updateUserCategory = async (
+  id: number,
+  category: Category
+): Promise<Category> => {
   return http.put(`/categories/${id}`, { ...category }).then(extract);
 };
