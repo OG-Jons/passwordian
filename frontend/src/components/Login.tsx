cd31c062b2065551536d5e0274e131c0c616f73f
Sl1van <nomail>
2022-07-03 19:43:24 +0200

    added logic to the login component

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,8 +1,25 @@
 import { Button, TextField } from "@mui/material";
+import { useState } from "react";
 import { Navigate } from "react-router-dom";
+import { login } from "../services/APIService";
 import { isAuthenticated } from "../services/AuthService";
 
 function Login() {
+  const [username, setUsername] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const handleUsernameChange = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setUsername(e.target.value);
+  };
+
+  const handlePasswordChange = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setPassword(e.target.value);
+  };
+
   if (isAuthenticated()) {
     return <Navigate to="/passwords" replace />;
   } else {
@@ -18,17 +35,19 @@ function Login() {
       >
         <TextField
           style={{ margin: 5 }}
-          id="outlined-basic"
+          id="username"
           label="Username"
           variant="outlined"
+          onChange={handleUsernameChange}
         />
         <TextField
           style={{ margin: 5 }}
-          id="outlined-password-input"
+          id="password"
           label="Password"
           type="password"
+          onChange={handlePasswordChange}
         />
-        <Button style={{ margin: 5 }} variant="outlined">
+        <Button style={{ margin: 5 }} onClick={() => login(username, password)} variant="outlined">
           Login
         </Button>
       </div>
