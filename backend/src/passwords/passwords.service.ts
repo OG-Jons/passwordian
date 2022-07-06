e07aa1ded831d8813b2532b53969deec08c726a3
OG-Jons <jonas@marschall.pro>
2022-07-06 19:28:29 +0200

    fixed stuff

--- backend/src/passwords/passwords.service.ts
+++ backend/src/passwords/passwords.service.ts
@@ -24,12 +24,12 @@ export class PasswordsService {
     createPasswordDto: CreatePasswordDto,
     user: User,
   ): Promise<Password> {
-    const category = await this.categoryService.findOne(
-      createPasswordDto.categoryId,
-    );
-
     let newPassword = new Password();
-    newPassword.category = category;
+    if (createPasswordDto.categoryId) {
+      newPassword.category = await this.categoryService.findOne(
+        createPasswordDto.categoryId,
+      );
+    }
     newPassword.user = user;
     newPassword = { ...newPassword, ...createPasswordDto };
 
@@ -41,7 +41,6 @@ export class PasswordsService {
 
   async findAllFromUser(user: User): Promise<Password[]> {
     return await this.passwordRepository.findBy({
-      username: user.username,
       user: { id: user.id },
     });
   }
@@ -64,10 +63,10 @@ export class PasswordsService {
     user: User,
   ): Promise<UpdateResult> {
     if (!(await this.checkIfPasswordExists(id))) {
-      throw new NotFoundException('Category not found');
+      throw new NotFoundException('Password not found');
     }
-    if (await this.checkIfUserHasPassword(user.id, id)) {
-      throw new ForbiddenException('This is not your category');
+    if (!(await this.checkIfUserHasPassword(user.id, id))) {
+      throw new ForbiddenException('This is not your password');
     }
 
     return await this.passwordRepository.update(id, updatePasswordDto);
@@ -75,10 +74,10 @@ export class PasswordsService {
 
   async remove(id: number, user: User): Promise<DeleteResult> {
     if (!(await this.checkIfPasswordExists(id))) {
-      throw new NotFoundException('Category not found');
+      throw new NotFoundException('Password not found');
     }
-    if (await this.checkIfUserHasPassword(user.id, id)) {
-      throw new ForbiddenException('This is not your category');
+    if (!(await this.checkIfUserHasPassword(user.id, id))) {
+      throw new ForbiddenException('This is not your password');
     }
 
     return await this.passwordRepository.delete(id);
@@ -93,10 +92,14 @@ export class PasswordsService {
         throw new BadRequestException('Password ID is required');
       }
       if (!(await this.checkIfPasswordExists(password.id))) {
-        throw new NotFoundException('Category not found');
+        throw new NotFoundException(
+          `Password with id #${password.id} not found`,
+        );
       }
-      if (await this.checkIfUserHasPassword(user.id, password.id)) {
-        throw new ForbiddenException('This is not your category');
+      if (!(await this.checkIfUserHasPassword(user.id, password.id))) {
+        throw new ForbiddenException(
+          'This is not your password: ' + password.id,
+        );
       }
     }
 
