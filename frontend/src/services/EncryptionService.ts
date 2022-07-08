29050ca1ee1e4e9d0c48e209c974c9cee6584343
Sl1van <sl1van@sl1van.com>
2022-07-08 09:03:59 +0200

    added encrypt/decrypt methods for commonly used functions that interact with the backend

--- frontend/src/services/EncryptionService.ts
+++ frontend/src/services/EncryptionService.ts
@@ -1,20 +1,44 @@
-import { Password } from "../types";
+import { Category, Password } from "../types";
 
 import sjcl from "sjcl";
+import { createUserPassword, getPassword, getUserCategories, updateUserPassword } from "./APIService";
 
-const decyptPassword = (password: Password, masterPassword: string): string => {
+const decyptPassword = (password: Password, masterPassword: string): Password => {
   if (password.password) {
-    return sjcl.decrypt(masterPassword, JSON.parse(password.password));
+    const decrypted: string = sjcl.decrypt(masterPassword, JSON.parse(password.password));
+    return {...password, password: decrypted};
   }
-  return "";
+  return password;
 };
 
-const encyptPassword = (password: Password, masterPassword: string): string => {
+const encyptPassword = (password: Password, masterPassword: string): Password => {
   if (password.password) {
-    return JSON.stringify(sjcl.encrypt(
-        masterPassword,
-        password.password
-      ))
+    const encrypted: string = JSON.stringify(sjcl.encrypt(
+      masterPassword,
+      password.password
+    ))
+    return {...password, password: encrypted}
   }
-  return "";
+  return password;
 };
+
+const getDecryptedUserCategories = async (masterPassword: string): Promise<Category[]>{
+  const categories: Category[] = await getUserCategories();
+  return categories.map((category: Category) => {
+    category.passwords = category.passwords.map((password: Password) => decyptPassword(password, masterPassword))
+    return category
+  })
+}
+
+const getDecryptedPassword = async (id: number, masterPassword: string)=>{
+  const password: Password = await getPassword(id);
+  return decyptPassword(password, masterPassword);
+}
+
+const updateUserPasswordAndEncrypt = async (id: number, password: Password, masterPassword: string)=>{
+  return updateUserPassword(id, encyptPassword(password, masterPassword))
+}
+
+const createEncryptedUserPassword = async (id: number, password: Password, masterPassword: string)=>{
+  return createUserPassword(encyptPassword(password, masterPassword));
+}
